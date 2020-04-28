53f7dbc365843dd34f9010988bf95cde638deb40
Robby Gibrant <42002947+gibrant@users.noreply.github.com>
2020-04-28 15:12:30 +0700

    Javascript
    In this file contain script function for Navbar Side but will active in mobile mode

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Make a function for Navbar button
+$(document).ready(function() {
+    $("#menu-open").click(function() {
+        $(".nav-side .navbar-menu").toggleClass("sidebar-open");
+    });
+});
+  
